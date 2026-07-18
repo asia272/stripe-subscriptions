4ba1574cf65321ad7b7f757c67243866dfd6663c
asia272 <asiaashraf7272@gmail.com>
2026-07-18 16:49:59 +0500

    complete  hero se citon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+
+import { Hero } from "@/components/Hero";
+import { Pricing } from "@/components/Pricing";
+
 
 
 export default function Home() {
