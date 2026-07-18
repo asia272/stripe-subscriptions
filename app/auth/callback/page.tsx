acc2eecd5d6344a8ce5b7c07bc5218b610dad0f0
asia272 <asiaashraf7272@gmail.com>
2026-07-18 13:24:54 +0500

    add user auth

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { checkAuthStatus } from "@/app/actions/auth.actions";
+
+export default function AuthPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        async function authenticate() {
+            const result = await checkAuthStatus();
+
+            if (result.success) {
+                router.replace("/");
+            } else {
+                router.replace("/login");
+            }
+        }
+
+        authenticate();
+    }, [router]);
+
     return (
-        <div>
-            <h1>vallback bpage will be display here</h1>
-        </div>
-    )
-}
+        <div className="flex h-screen items-center justify-center">
+            <div className="text-center">
+                <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-300 border-t-black mx-auto"></div>
 
-export default page
+                <p className="mt-4 text-gray-600">
+                    Signing you in...
+                </p>
+            </div>
+        </div>
+    );
+}
